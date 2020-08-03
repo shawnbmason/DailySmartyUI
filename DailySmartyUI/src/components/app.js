0c413d5fdcd0f39e61a4b889b300ce9381b54595
shawnbmason <s.mason1993@yahoo.com>
2020-08-03 19:33:01 -0400

    generated project

---
+++ DailySmartyUI/src/components/app.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <div>logo</div>
+        <h1>DevCamp React Starter</h1>
+        <h2>React Redux Router</h2>
+      </div>
+    );
+  }
+}
