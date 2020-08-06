f4fb41eeb8b26032ed112f60441f10f2b24ea9b3
shawnbmason <s.mason1993@yahoo.com>
2020-08-06 19:22:10 -0400

    setup logo component and corresponding styles

--- DailySmartyUI/src/components/app.js
+++ DailySmartyUI/src/components/app.js
@@ -1,10 +1,12 @@
-import React, { Component } from 'react';
+ import React, { Component } from 'react';
+ import Logo from './logo';
 
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
         <div>logo</div>
+        <Logo/>
         <h1>DevCamp React Starter</h1>
         <h2>React Redux Router</h2>
       </div>
